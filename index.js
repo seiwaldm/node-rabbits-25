614991a7ed52b8e6cb531dd0f37fdc9fd166d2e8
seiwaldm <seiwald.markus@johak.at>
2025-09-29 17:17:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // App.js
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); //import cors package
 const rabbitRoutes = require("./rabbits/routes/rabbits.routes");
 
 const app = express();
 const port = 7070;
 
-app.use(cors());
+app.use(cors()); //set the cors header for all request-origins! not recommended for production!
 
 /* Routes */
 app.use("/rabbits", rabbitRoutes);
